c8acfc48c041d8c3b40724b18bd3ba54d0bcff0c
Memo Chou <memochou1993@gmail.com>
2021-11-29 00:16:34 +0800

    Send email when miner in stock

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"log"
+	"net/smtp"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 const (
@@ -21,7 +25,29 @@ func main() {
 			if strings.ToUpper(availability) == strings.ToUpper("Out of stock") {
 				return
 			}
+			notify()
 		})
 		c.Visit(target)
+	}
+}
+
+func notify() {
+	addr := "smtp.gmail.com:587"
+	host := "smtp.gmail.com"
+	identity := ""
+	from := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+	to := os.Getenv("SMTP_USERNAME")
+	subject := "RAK Hotspot Miner in stock now!"
+	body := ""
+	msg := "From:" + from + "\r\n" + "To:" + to + "\r\n" + "Subject:" + subject + "\r\n" + body
+	if err := smtp.SendMail(
+		addr,
+		smtp.PlainAuth(identity, from, password, host),
+		from,
+		[]string{to},
+		[]byte(msg),
+	); err != nil {
+		log.Println(err)
 	}
 }
